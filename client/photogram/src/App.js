b2e1f13152eab9816c7c80e2b4c527680190451a
geomgl <gmagloire22@gmail.com>
2022-06-11 14:07:57 -0400

    refactored navbar into component

--- client/photogram/src/App.js
+++ client/photogram/src/App.js
@@ -1,38 +1,12 @@
 
 import React from 'react'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div id='App' className='full-height'>
-      <div id='navBarContainer'>
-        <div id='navBar' className='flex'>
-          <div id='navBarLogo'>Photogram</div>
-          <div id='searchBarContainer'>
-            <input id='searchBar' placeholder='Search'>
-            </input>
-          </div>
-          <div id='navBarIconContainer' className='flex'>
-            <div>
-              <i className='bi-house navBarIcon'></i>
-            </div>
-            <div>
-              <i className='bi-inbox-fill navBarIcon'></i>
-            </div>
-            <div>
-              <i className='bi-plus-square navBarIcon'></i>
-            </div>
-            <div>
-              <i className='bi-compass navBarIcon'></i>
-            </div>
-            <div>
-              <i className='bi-heart navBarIcon'></i>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <NavBar />
       <div id='mainContainer'></div>
-
     </div>
   );
 }
